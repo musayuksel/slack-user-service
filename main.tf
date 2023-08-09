959ea55a4a549788e59aba889968d2887341bed8
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-09 15:43:43 +0100

    Add terraform template

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+}
+
+# Create a VPC
+resource "aws_cognito_user_pool" "slack_user_pool" {
+  name                     = "slack_users"
+  username_attributes      = ["email"]
+  auto_verified_attributes = ["email"]
+
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = false
+    require_numbers   = false
+    require_symbols   = false
+    require_uppercase = false
+  }
+
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+  }
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_message        = "Your verification code is {####}."
+    email_subject        = "Your verification code"
+
+  }
+
+  schema {
+    name                     = "email"
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    required                 = true
+    string_attribute_constraints {
+      min_length = 0
+      max_length = 256
+    }
+  }
+
+
+}
